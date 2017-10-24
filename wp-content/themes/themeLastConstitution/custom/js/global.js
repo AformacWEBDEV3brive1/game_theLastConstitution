20c3e63b69831868eba6041a114b826a429f0760
ezaltar <ezaltar@ezaltar-HP-ProBook-450-G3>
2017-10-24 08:12:39 +0200

    ajout de plein de joueurs possible

--- wp-content/themes/themeLastConstitution/custom/js/global.js
+++ wp-content/themes/themeLastConstitution/custom/js/global.js
@@ -2,7 +2,6 @@ function move(id) {
     //alert('x='+ id.className.split(' ')[1] + ' y=' + id.className.split(' ')[3]);
     var x = id.className.split(' ')[1];
     var y = id.className.split(' ')[3];
-    //for (j=1 ; j<=2 ; j++){
             $.ajax({url: 'wp-content/plugins/game_plugin/process_general.php',
             type: 'post',
             data: {info: 'move', new_position: x + ";" + y},
@@ -11,7 +10,6 @@ function move(id) {
                 
             }
         });
-    //}
 }
 
 
