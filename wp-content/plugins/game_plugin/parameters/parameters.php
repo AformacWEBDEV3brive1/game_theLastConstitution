0fc2cfd68deeb5984733f41ef2c4e26077f13d78
christian <christian.manach@hotmail.fr>
2017-12-07 08:16:40 +0100

    fonction create_party 107%

--- wp-content/plugins/game_plugin/parameters/parameters.php
+++ wp-content/plugins/game_plugin/parameters/parameters.php
@@ -1,6 +1,6 @@
 <?php
 
-$connexion_string = "mysql:dbname=game_last_constitution;host=127.0.0.1;charset=utf8";
+$connexion_string = "mysql:dbname=gameTheLastConstitution;host=127.0.0.1;charset=utf8";
 
 $login = "root";
 $mdp = "123456789$";
