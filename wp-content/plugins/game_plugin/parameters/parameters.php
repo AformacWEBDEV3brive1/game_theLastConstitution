cc45fe2033fd01b02efc24aa3a8f4dc140eb6ca5
ikenn <ikenn@ikenn-HP-ProBook-450-G3>
2017-12-08 11:44:53 +0100

    background qui ne bouge plus

--- wp-content/plugins/game_plugin/parameters/parameters.php
+++ wp-content/plugins/game_plugin/parameters/parameters.php
@@ -1,8 +1,8 @@
 <?php
-$connexion_string = "mysql:dbname=gameTheLastConstitution;host=127.0.0.1;charset=utf8";
+$connexion_string = "mysql:dbname=game_theLastConstitution_wp;host=127.0.0.1;charset=utf8";
 
 $login = "root";
-$mdp = "mega6*3zd";
+$mdp = "limogescsp";
 
 function openBDD()
 {
