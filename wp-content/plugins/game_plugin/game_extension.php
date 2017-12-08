4777726be6455939529759222bde405071f40350
OmegaZord <acharles0111@gmail.com>
2017-12-08 12:04:18 +0100

    modif game_extension

--- wp-content/plugins/game_plugin/game_extension.php
+++ wp-content/plugins/game_plugin/game_extension.php
@@ -409,10 +409,7 @@ INSERT INTO `games_metadata` (`id_partie`, `start`) VALUES
 (1, CURRENT_TIMESTAMP),
 (2, CURRENT_TIMESTAMP),
 (3, CURRENT_TIMESTAMP),
-(4, CURRENT_TIMESTAMP),
-(5, CURRENT_TIMESTAMP),
-(6, CURRENT_TIMESTAMP),
-(7, CURRENT_TIMESTAMP);
+(4, CURRENT_TIMESTAMP);
 
 INSERT INTO `games_data` (`id_joueur`, `id_partie`, `position`, `points_action`, `equipe`) VALUES
 (1, 1, '7;3', 15, 1),
@@ -485,6 +482,8 @@ INSERT INTO `type_batiments` (`type`, `nom`) VALUES
 (2, 'mairie'),
 (3, 'maison'),
 (4, 'hopital');
+
+
 
 INSERT INTO `chat` (`id`, `id_joueur`, `position`, `equipe`, `id_partie`, `tag`, `message`, `heure`) VALUES
 (1, 1, '0;0', 1, 1, 'ville', 'bonjour', '1511172044'),
